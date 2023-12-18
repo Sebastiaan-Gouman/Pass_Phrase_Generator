9dd3d4eb1119365e3368a16ae37ee8ff29445331
Sebastiaan <sebastiaan.gouman@hva.nl>
2023-12-18 17:33:52 +0100

    Geen

--- Pass Phrase Generator.py
+++ Pass Phrase Generator.py
@@ -1,15 +1,17 @@
 #.-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-. To-Do List:
 #|                                                                       | 1. Add Special characters
-#|                 # Pass Phrase Generator                               | 2. Make order of items random (ie. not start with a color every time)
-#|                 # Created By Sebastiaan                               | 3. Add input option to choose amount of items in pass phrase
-#|                                                                       | 4. Use safe random module
-#|                Intended for educational use only                      | 5. Use JSON files for items in lists
-#!          do NOT use as a real life Pass Phrase Generator              ! 6. Copy Pass Phrase to clipboard automatically
-#:                                                                       : 7. Add input option for item language
+#|                 # Pass Phrase Generator                               | 2. Add input option to choose amount of items in pass phrase
+#|                 # Created By Sebastiaan                               | 3. Use safe random module
+#|                                                                       | 4. Use JSON files for items in lists
+#|                Intended for educational use only                      | 5. Add input option for item language
+#!          do NOT use as a real life Pass Phrase Generator              ! 6. Add a function for something
+#:                                                                       :
 #`-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-etf-'
 
 # Import random module for generating random numbers
+# Import pyperclip for copying pass phrase to clipboard automatically (https://pypi.org/project/pyperclip/)
 import random
+import pyperclip
 
 # Define list, define random number to use in print statement starting at 0 ending at list length -1.
 kleur = ["Rood", "Wit", "Blauw", "Groen", "Geel", "Paars"]
@@ -21,8 +23,27 @@ number2 = random.randint(0,3)
 number3 = random.randint(0,99)
 str_number = str(number3)
 
+# Create a list with the variables
+variables = [kleur[number], dier[number2], str(str_number)]
 
-print((kleur[number])+(dier[number2])+str_number)
+# Shuffle the order of variables
+random.shuffle(variables)
+
+# Concatenate the variables and print the result
+result = "".join(variables)
+print("Copied to clipboard:", result)
+
+pyperclip.copy(result)
+
+#print((kleur[number])+(dier[number2])+str_number)
+#pyperclip.copy((kleur[number])+(dier[number2])+str_number)
+
+
+
+
+
+
+
 
 
 
